8b53c257b301d38fc48004ebd4c174fe20fef090
parsuyapritam <parsuyapritam@gmail.com>
2025-04-27 23:36:31 +0530

    Add files via upload

---
+++ Assigment01.py
@@ -0,0 +1,23 @@
+#task 1
+a = input ("Enter a first number: ")
+b=input("Enter a second number: ")
+
+a=int(a)
+b=int(b)
+
+add=a+b
+sub=a-b
+mul=a*b
+div=a/b
+
+print("Addition: ",add)
+print("Subtraction: ",sub)
+print("Multiplication: ",mul)
+print("Division: ",div)
+
+#task 2
+
+frname=input("\nEnter you first name: ")
+lname=input("Enter you last name: ")
+
+print("Hello, " + frname,lname + "! Welcome to the Python program.")
